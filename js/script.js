e38327daccb113c1f12a4880146eff333f06b657
William Högman <william@hogman.me>
2013-05-27 17:14:51 +0200

    replaced google's font loader with a plain old stylesheet

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
 (function() {
 
-  window.WebFontConfig = {
-    google: { families: [ 'Open+Sans::latin' ] }
-  };
-
   $(function() {
     var tumblr_key = "3tzcDAyoP9zRbi67ddiLWCfbswUjOSu49A8pnLQQWEuQp4eMuL";
     $.ajax(
@@ -28,5 +24,4 @@
   };
   add_script(('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js');
   add_script('//platform.twitter.com/widgets.js');
-  add_script("//ajax.googleapis.com/ajax/libs/webfont/1/webfont.js");
 })();
